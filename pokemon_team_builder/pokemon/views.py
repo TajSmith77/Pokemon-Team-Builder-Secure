aecf07ef8805467f57c1d8cd2eb5d23d740ffbf2
Taj Smith <TajSmith@Deloitte.com>
2025-01-17 10:21:32 -0500

    quick fix

--- pokemon_team_builder/pokemon/views.py
+++ pokemon_team_builder/pokemon/views.py
@@ -13,10 +13,10 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
-from django.contrib.auth.tokens import default_token_generator, ExpiringTokenGenerator
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
-from .utils import *
+from .utils import ExpiringTokenGenerator
 from .models import *
 from .forms import *
 import csv
